a9c5cdaf03cc17f178501ae8c550ffda043d1c94
splunklearners1 <76473925+splunklearners1@users.noreply.github.com>
2024-07-21 09:22:35 +0000

    splunk install
    splunk install

---
+++ autosplunk.sh
@@ -0,0 +1,1 @@
+./splunkinstall.sh
---
+++ splunkinstall.sh
@@ -0,0 +1,27 @@
+## To become root to install splunk on ec2
+#sudo su - 
+
+## Download Splunk rpm from splunk portal, you need to update this link for latest splunk software, refer slide 3 and 4
+
+wget -O wget -O splunk-9.2.1-78803f08aabb.x86_64.rpm "https://download.splunk.com/products/splunk/releases/9.2.1/linux/splunk-9.2.1-78803f08aabb.x86_64.rpm"
+
+##Create dedicated mount point/folder for splunk installation
+mkdir /splunk
+
+##Install splunk
+rpm -ivh --prefix /splunk splunk-9.2.1-78803f08aabb.x86_64.rpm
+## Go to splunk binary path
+cd /splunk/splunk/bin
+
+## Start splunk service, and feed password along with command, you can change password once you login to splunk portal 
+./splunk start --accept-license --answer-yes --no-prompt --seed-passwd Admeensd@7658
+
+## Enable autostart of splunk service
+./splunk enable boot-start
+## Fix minimum free diskspace issue - only for lab environment not for production
+echo "[diskUsage]" >> /splunk/splunk/etc/system/local/server.conf
+echo "minFreeSpace = 50" >> /splunk/splunk/etc/system/local/server.conf
+/splunk/splunk/bin/splunk restart
+## Check splunk status
+./splunk status
+echo "installation completed"
